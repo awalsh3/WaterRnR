1b633f6cb5cc38e84d53b7f420894841d5fd3408
Annie Walsh <annie@anyonething.co.uk>
2021-05-08 16:40:41 +0100

    bookings model route controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_08_145451) do
+ActiveRecord::Schema.define(version: 2021_05_08_153001) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.datetime "date"
+    t.boolean "confirmed", default: false
+    t.bigint "user_id", null: false
+    t.bigint "pool_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["pool_id"], name: "index_bookings_on_pool_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "pools", force: :cascade do |t|
     t.string "location"
@@ -37,5 +48,7 @@ ActiveRecord::Schema.define(version: 2021_05_08_145451) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "pools"
+  add_foreign_key "bookings", "users"
   add_foreign_key "pools", "users"
 end
