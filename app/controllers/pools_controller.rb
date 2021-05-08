a350e27f2975d2c6805f8ab5833e9d8ddf268286
Annie Walsh <annie@anyonething.co.uk>
2021-05-08 16:20:04 +0100

    pools controller, update pages_controller skip, routes remove user

---
+++ app/controllers/pools_controller.rb
@@ -0,0 +1,10 @@
+class PoolsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
+
+  def index
+  end
+
+  def show
+  end
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users do
-    resources :pools, only: [ :new, :create, :index ]
-  end
+    resources :pools, only: [ :new, :create, :index. :show ]
 end
