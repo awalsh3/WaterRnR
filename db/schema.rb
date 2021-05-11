d846ea3e721624efe20c210ab152d63070178158
Arthur Maroquene Froissart <arthurmf.amf@gmail.com>
2021-05-11 21:21:41 +0100

    added sydney pool picture

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_08_153001) do
+ActiveRecord::Schema.define(version: 2021_05_08_154645) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,15 @@ ActiveRecord::Schema.define(version: 2021_05_08_153001) do
     t.index ["user_id"], name: "index_pools_on_user_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.string "comment"
+    t.string "rating"
+    t.bigint "booking_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["booking_id"], name: "index_reviews_on_booking_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -51,4 +60,5 @@ ActiveRecord::Schema.define(version: 2021_05_08_153001) do
   add_foreign_key "bookings", "pools"
   add_foreign_key "bookings", "users"
   add_foreign_key "pools", "users"
+  add_foreign_key "reviews", "bookings"
 end
