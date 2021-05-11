6c9ed845b4a8fb3b5115e15861eb6b1c26d5e4d9
David Morton <dave.mort@hotmail.com>
2021-05-11 20:26:27 +0100

    update

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,10 +1,13 @@
 class BookingsController < ApplicationController
   def new
+    @booking = Booking.new
   end
 
   def create
+
   end
 
   def show
+    @booking = booking.find(params[:id])
   end
 end
