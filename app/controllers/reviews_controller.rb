53faa47976b8e81f8383b53b51ee9abf6ff51800
Annie Walsh <annie@anyonething.co.uk>
2021-05-11 18:13:55 +0100

    rview first stab

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,5 +3,19 @@ class ReviewsController < ApplicationController
   end
 
   def create
+    @review = Review.new(review_params)
+    @pool = pool.find(params[:pool_id])
+    @review.pool = @list
+    if @review.save
+      redirect_to list_path(@pool)
+    else
+      render 'pools/show'
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:comment, :rating)
   end
 end
