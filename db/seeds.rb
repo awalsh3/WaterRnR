e18606b8871158ade9257cd37ce58fd3e4e7608f
Annie Walsh <annie@anyonething.co.uk>
2021-05-13 16:38:31 +0100

    homepage: nav, footer, banner shell

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning up database'
+Pool.destroy_all
+puts 'Database is clean'
+puts 'Creating 5 fake pools...'
+5.times do
+  pool = Pool.create(
+    location: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+    description: Faker::Quotes::Shakespeare.romeo_and_juliet_quote)
+    # photo:"https://images.unsplash.com/photo-1543489822-c49534f3271f?ixid=MnwxMjA3fDB8MHxwaG90by1[…]GVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2978&q=80")
+  pool.save!
+end
+puts 'Finished!'
