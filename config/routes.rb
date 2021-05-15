b59cbb75ce588db00e62428fd2e875ce739e2082
Annie Walsh <annie@anyonething.co.uk>
2021-05-15 12:19:19 +0100

    delete this branch please

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-    resources :pools, only: [ :new, :create, :index, :show ]
-    resources :bookings, only: [ :new, :create, :show ] do
+    resources :pools, only: [ :new, :create, :index, :show ] do
+      resources :bookings, only: [ :new, :create, :show ] do
         resources :reviews, only: [:new, :create]
     end
+  end
 end
