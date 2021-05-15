8c5e84e853313b9bc51c76214372f9f956902df8
Annie Walsh <annie@anyonething.co.uk>
2021-05-15 15:52:15 +0100

    review syntax error

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,16 +3,16 @@ class ReviewsController < ApplicationController
     @review = Review.new
   end
 
-  def create
-    @review = Review.new(review_params)
-    @review.user = current_user
-    @review.@pool = Pool.find(params[:pool_id])
-    if @review.save
-      redirect_to pool_path(@pool)
-    else
-      render 'pools/show'
-    end
-  end
+  # def create
+  #   @review = Review.new(review_params)
+  #   @review.user = current_user
+  #   @review.@pool = Pool.find(params[:pool_id])
+  #   if @review.save
+  #     redirect_to pool_path(@pool)
+  #   else
+  #     render 'pools/show'
+  #   end
+  # end
 
   private
 
