645c811c1c53b69a33146c6d162ac4653ff46173
Arthur Maroquene Froissart <arthurmf.amf@gmail.com>
2021-05-18 19:43:46 +0100

    add price to pools

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_15_132115) do
+ActiveRecord::Schema.define(version: 2021_05_18_184226) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 2021_05_15_132115) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "price"
     t.index ["user_id"], name: "index_pools_on_user_id"
   end
 
