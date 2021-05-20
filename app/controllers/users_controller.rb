b7bfd5974a549eb9540b6ec7f82befb53e96327b
Annie Walsh <annie@anyonething.co.uk>
2021-05-20 19:43:22 +0100

    add many bookings pool and logic if no booking on pool

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
   def dashboard
     @bookings_user_made = current_user.bookings
-    @bookings_user_received = Booking.joins(:pool).where(user: current_user)
     @pools_user_own = Pool.where(user: current_user)
+    @bookings_user_received = []
+    @pools_user_own.each do |pool|
+      @bookings_user_received << pool.bookings
+    end
+      @bookings_user_received.flatten!
   end
 end
