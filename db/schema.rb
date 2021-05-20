d573535d10ea106ae767d45ed6bdba596c9cf8f6
David Morton <dave.mort@hotmail.com>
2021-05-20 21:57:35 +0100

    change

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_18_195304) do
+ActiveRecord::Schema.define(version: 2021_05_20_205554) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2021_05_18_195304) do
     t.string "price"
     t.string "title"
     t.text "photo"
+    t.datetime "date"
     t.index ["user_id"], name: "index_pools_on_user_id"
   end
 
